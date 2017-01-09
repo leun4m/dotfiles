9c038da79b59ac6188624a7e9515a67cd4877015
leun4m <leun4m@mail.de>
2017-01-09 21:03:03 +0100

    Change install.sh to copy files

--- install.sh
+++ install.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
 ########## Variables
-
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="vimrc zshrc minttyrc"    # list of files/folders to symlink in homedir
@@ -22,10 +17,10 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
     mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    echo "Copy $file in home directory."
+    cp $dir/$file ~/.$file
 done
